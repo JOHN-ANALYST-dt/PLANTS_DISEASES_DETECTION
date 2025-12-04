472e3f85d576b814ae05a232eee05252cb2a5521
john <john.nyangoya@student.moringaschool.com>
2025-12-04 23:45:13 +0300

    tr

--- WEB_APP.py
+++ WEB_APP.py
@@ -121,12 +121,6 @@ for fruit in FRUIT_CLASSES:
 fruit_html += "</ol>"
 
 
-#looping over each model plant type to create tabs
-for plant in VEGETABLE_CLASSES:
-    st.markdown(f"[Train {plant} Model](./APPS/{plant.lower()}.py)")
-
-for plant in FRUIT_CLASSES:
-    st.markdown(f"[Train {plant} Model](./APPS/{plant.lower()}.py)")    
 
 # --- 5. LOAD MODEL ---
 @st.cache_resource
